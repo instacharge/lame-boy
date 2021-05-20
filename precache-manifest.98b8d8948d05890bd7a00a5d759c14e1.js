ab6d286cf3eb4697160f957af783fc911eb9fbd3
Kyle James <kyle.james1@dcmail.ca>
2021-05-20 15:49:15 -0400

    Updates

---
+++ precache-manifest.98b8d8948d05890bd7a00a5d759c14e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4fd6901cee21785d5c042903b4eeda",
+    "url": "/lame-boy/index.html"
+  },
+  {
+    "revision": "63105088f9d9b66fff11",
+    "url": "/lame-boy/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "5f57d7b45af0d7ff2795",
+    "url": "/lame-boy/static/css/main.071cc663.chunk.css"
+  },
+  {
+    "revision": "63105088f9d9b66fff11",
+    "url": "/lame-boy/static/js/2.c59745d8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/lame-boy/static/js/2.c59745d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f57d7b45af0d7ff2795",
+    "url": "/lame-boy/static/js/main.dfe1ad07.chunk.js"
+  },
+  {
+    "revision": "fe48c91c2a2506280823",
+    "url": "/lame-boy/static/js/runtime-main.f0f99780.js"
+  },
+  {
+    "revision": "3054219569e95f1806a0825a7b547aa1",
+    "url": "/lame-boy/static/media/button-cart-scan.30542195.png"
+  },
+  {
+    "revision": "f674e2adac291ab4ce6935798bd8c84e",
+    "url": "/lame-boy/static/media/button-contact-scan.f674e2ad.png"
+  },
+  {
+    "revision": "4fa60e1cc4dfb386aa80055121e175b3",
+    "url": "/lame-boy/static/media/button-media-scan.4fa60e1c.png"
+  },
+  {
+    "revision": "a69435f1a94b141f892c1afa4228db9b",
+    "url": "/lame-boy/static/media/enter-your-email.a69435f1.jpg"
+  },
+  {
+    "revision": "6243701ea528a1751ea2492e29997a6d",
+    "url": "/lame-boy/static/media/lame-boy-logo-scan.6243701e.jpg"
+  }
+]);
